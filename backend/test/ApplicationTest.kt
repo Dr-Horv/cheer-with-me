be722131d0e950d70dcb6fa34494742b56f20d5e
Johan Horvius <drhorv@gmail.com>
2019-06-20 21:46:12 +0200

    Make code change

--- backend/test/ApplicationTest.kt
+++ backend/test/ApplicationTest.kt
@@ -18,7 +18,7 @@ class ApplicationTest {
         withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Cheers mate!", response.content)
+                assertEquals("Cheers mate! :D", response.content)
             }
         }
     }
