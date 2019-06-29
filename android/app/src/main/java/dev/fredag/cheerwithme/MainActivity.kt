4a22a853d66436c1f8f8b8356cc8ab9161e5f025
Joel Gustafsson <me@joelgustafsson.com>
2019-06-29 11:58:54 +0200

    Send request on clicking on a drink

--- android/app/src/main/java/dev/fredag/cheerwithme/MainActivity.kt
+++ android/app/src/main/java/dev/fredag/cheerwithme/MainActivity.kt
@@ -2,9 +2,13 @@ package dev.fredag.cheerwithme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.core.view.children
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,9 +22,31 @@ class MainActivity : AppCompatActivity() {
         val iconFont = FontManager.getTypeface(applicationContext, FontManager.FONTAWESOME)
         FontManager.markAsIconContainer(icons_container, iconFont)
 
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://cheer-with-me.fredag.dev/"
+
+
         for (child in icons_container.children) {
             if (child is Button) {
-                child.setOnClickListener { println(child.tag) }
+                child.setOnClickListener { view ->
+
+                    println(view.tag)
+
+
+                    val stringRequest = StringRequest(Request.Method.GET, url,
+                        Response.Listener<String> { response ->
+                            // Display the first 500 characters of the response string.
+                            println("Response: %s".format(response.toString()))
+                        },
+                        Response.ErrorListener { error ->
+                            // TODO: Handle error
+                            println(error.message)
+                            println("That didn't work!")
+                        })
+                    queue.add(stringRequest)
+
+
+                }
             }
         }
     }
