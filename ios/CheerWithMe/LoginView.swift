b26906c329601d0c053bd0809eef0bc2b70dcbb9
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-07-22 23:44:14 +0200

    Use new iOS 14 API

--- ios/CheerWithMe/LoginView.swift
+++ ios/CheerWithMe/LoginView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct LoginView : View {
     @State private var username: String = ""
@@ -28,8 +29,42 @@ struct LoginView : View {
             }
             .padding()
             
-            AuthenticationButton()
-                .frame(width: 300, height: 50)
+            SignInWithAppleButton(.signIn,
+                onRequest: { request in
+                    request.requestedScopes = [.fullName, .email]
+                },
+                onCompletion: { result in
+                    switch result {
+                    case .success (let authResult):
+                        print("Authorization successful.")
+                        print(authResult)
+                        if let appleIDCredential = authResult.credential as? ASAuthorizationAppleIDCredential {
+                            let userIdentifier = appleIDCredential.user
+                            let fullName = appleIDCredential.fullName
+                            let email = appleIDCredential.email
+                            
+                            let identityToken = appleIDCredential.identityToken.flatMap { String(data: $0, encoding: .utf8) }
+                            let authorizationCode = appleIDCredential.authorizationCode.flatMap { String(data: $0, encoding: .utf8) }
+                            
+                            print(userIdentifier, fullName as Any, email as Any, identityToken as Any, authorizationCode as Any)
+                            
+                            BackendService.shared.token = identityToken
+                            
+                            guard let code = authorizationCode else {
+                                preconditionFailure("authorizationCode must be defined")
+                            }
+                            
+                            let _ = BackendService.shared.register(payload: .init(code: code)).sink(receiveCompletion: { completion in
+                                print(completion)
+                            }, receiveValue: { response in
+                                print(response)
+                            })
+                        }
+                    case .failure (let error):
+                        print("Authorization failed: \(error.localizedDescription)")
+                    }
+                }
+            ).frame(width: 300, height: 50, alignment: .center).signInWithAppleButtonStyle(.white)
             
             Button(action: {
                 self.onPressSignIn(self.username)
