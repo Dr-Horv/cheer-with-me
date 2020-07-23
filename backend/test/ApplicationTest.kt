5f19740ef16d24a094093086d2388ececacf868e
Johan Horvius <drhorv@gmail.com>
2020-07-23 15:16:10 +0200

    Fix test

--- backend/test/ApplicationTest.kt
+++ backend/test/ApplicationTest.kt
@@ -1,14 +1,23 @@
 package dev.fredag
 
 import dev.fredag.cheerwithme.module
+import io.ktor.config.MapApplicationConfig
 import io.ktor.http.*
 import kotlin.test.*
 import io.ktor.server.testing.*
+import io.ktor.util.KtorExperimentalAPI
 
 class ApplicationTest {
+    @KtorExperimentalAPI
     @Test
     fun testRoot() {
-        withTestApplication({ module(testing = true) }) {
+        withTestApplication({
+            (environment.config as MapApplicationConfig).apply {
+                put("push.pushArnIOS", "NONE")
+                put("push.pushArnAndroid", "NONE")
+            }
+            module(testing = true)
+        }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("Cheers mate! :D", response.content)
