3a5b0555771e534f13a4225456a93a10078faaea
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-07-23 16:56:08 +0200

    Add signing in ActivityIndicator

--- ios/CheerWithMe/LoginView.swift
+++ ios/CheerWithMe/LoginView.swift
@@ -11,8 +11,46 @@ import AuthenticationServices
 
 struct LoginView : View {
     @State private var username: String = ""
+    @State private var signingIn: Bool = false
     
     var setSignedIn: (Bool) -> Void
+    
+    
+    func handleAuthResult(_ result: Result<ASAuthorization, Error>) -> Void {
+        self.signingIn = true
+        
+        switch result {
+        case .success (let authResult):
+            print("Authorization successful.")
+            print(authResult)
+            if let appleIDCredential = authResult.credential as? ASAuthorizationAppleIDCredential {
+                let userIdentifier = appleIDCredential.user
+                let fullName = appleIDCredential.fullName
+                let email = appleIDCredential.email
+                
+                let identityToken = appleIDCredential.identityToken.flatMap { String(data: $0, encoding: .utf8) }
+                let authorizationCode = appleIDCredential.authorizationCode.flatMap { String(data: $0, encoding: .utf8) }
+                
+                print(userIdentifier, fullName as Any, email as Any, identityToken as Any, authorizationCode as Any)
+                
+                BackendService.shared.token = identityToken
+                
+                guard let code = authorizationCode else {
+                    preconditionFailure("authorizationCode must be defined")
+                }
+                
+                BackendService.shared.register(payload: .init(code: code, nick: username)) { response in
+                    print(response)
+                    BackendService.shared.token = response.accessToken
+                    self.signingIn = false
+                    self.setSignedIn(true)
+                }
+            }
+        case .failure (let error):
+            self.signingIn = false
+            print("Authorization failed: \(error.localizedDescription)")
+        }
+    }
     
     var body: some View {
         VStack {
@@ -23,48 +61,26 @@ struct LoginView : View {
                 Text("CheerWithMe").font(.largeTitle)
             }
             
-            HStack {
-                TextField("Username", text: $username).textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-            }
-            .padding()
-            
-            SignInWithAppleButton(.signIn,
-                onRequest: { request in
-                    request.requestedScopes = [.fullName, .email]
-                },
-                onCompletion: { result in
-                    switch result {
-                    case .success (let authResult):
-                        print("Authorization successful.")
-                        print(authResult)
-                        if let appleIDCredential = authResult.credential as? ASAuthorizationAppleIDCredential {
-                            let userIdentifier = appleIDCredential.user
-                            let fullName = appleIDCredential.fullName
-                            let email = appleIDCredential.email
-                            
-                            let identityToken = appleIDCredential.identityToken.flatMap { String(data: $0, encoding: .utf8) }
-                            let authorizationCode = appleIDCredential.authorizationCode.flatMap { String(data: $0, encoding: .utf8) }
-                            
-                            print(userIdentifier, fullName as Any, email as Any, identityToken as Any, authorizationCode as Any)
-                            
-                            BackendService.shared.token = identityToken
-                            
-                            guard let code = authorizationCode else {
-                                preconditionFailure("authorizationCode must be defined")
-                            }
-                            
-                            BackendService.shared.register(payload: .init(code: code, nick: username)) { response in
-                                print(response)
-                                BackendService.shared.token = response.accessToken
-                                self.setSignedIn(true)
-                            }
-                        }
-                    case .failure (let error):
-                        print("Authorization failed: \(error.localizedDescription)")
-                    }
+            if self.signingIn {
+                ProgressView("Signing in...").progressViewStyle(CircularProgressViewStyle())
+            } else {
+                HStack {
+                    TextField("Username", text: $username).textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
                 }
-            ).frame(width: 300, height: 50, alignment: .center).signInWithAppleButtonStyle(.white)
+                .padding()
+                
+                SignInWithAppleButton(.signIn,
+                    onRequest: { request in
+                        request.requestedScopes = []
+                    },
+                    onCompletion: { result in
+                        self.handleAuthResult(result)
+                    }
+                ).frame(width: 300, height: 50, alignment: .center).signInWithAppleButtonStyle(.white)
+            }
+            
+           
             
             Spacer()
         }
