23c40ddaba7c75f79f0300e1aa0892e78f9d98a4
Johan Horvius <drhorv@gmail.com>
2020-07-23 02:43:45 +0200

    Custom token auth WIP

---
+++ backend/src/web/Auth.kt
@@ -0,0 +1,54 @@
+package dev.fredag.cheerwithme.web
+
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.auth.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+
+
+class CheerWithMeAuthenticationProvider(configuration: Configuration) : AuthenticationProvider(configuration) {
+    internal val authenticationFunction = configuration.authenticationFunction
+
+    class Configuration internal constructor(name: String?) : AuthenticationProvider.Configuration(name) {
+        internal var authenticationFunction: AuthenticationFunction<String> = {
+            throw NotImplementedError(
+                "verifyToken function is not specified. Use basic { verifyToken { ... } } to fix."
+            )
+        }
+
+        /**
+         * Sets a validation function that will check given [String] token and return [Principal],
+         * or null if credential does not correspond to an authenticated principal
+         */
+        fun verifyToken(token: suspend ApplicationCall.(String) -> Principal?) {
+            authenticationFunction = token
+        }
+    }
+
+}
+
+fun Authentication.Configuration.cheerWithMe(
+    name: String? = null,
+    configure: CheerWithMeAuthenticationProvider.Configuration.() -> Unit
+) {
+    val provider = CheerWithMeAuthenticationProvider(CheerWithMeAuthenticationProvider.Configuration(name).apply(configure))
+    val verify = provider.authenticationFunction
+    provider.pipeline.intercept(AuthenticationPipeline.CheckAuthentication) { context ->
+        val authHeader = context.call.request.headers["authorization"]
+        if (authHeader == null || !authHeader.contains("bearer ", true)) {
+            call.respond(HttpStatusCode.Unauthorized)
+            context.challenge("cheerWithMeAuth", AuthenticationFailedCause.NoCredentials) {
+                call.respond(UnauthorizedResponse())
+                it.complete()
+            }
+        } else {
+            val token = authHeader.substring(0, "bearer ".length)
+            val principal = verify(call, token)
+            if (principal != null) {
+                context.principal(principal)
+            }
+        }
+    }
+    register(provider)
+}
