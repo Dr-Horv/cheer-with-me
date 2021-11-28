fc8cf80b0fd3fd4c2807249e15d68df9911ece4d
Pontus Malm <malm@chalmers.it>
2021-11-28 19:03:20 +0100

    Private views for friends

--- ios/MobileApp/MobileApp/Friends/FriendsView.swift
+++ ios/MobileApp/MobileApp/Friends/FriendsView.swift
@@ -10,45 +10,76 @@ struct FriendsView: View {
         List {
             Section(header: Text("FRIEND REQUESTS")) {
                 ForEach(viewModel.waitingFriends) { friend in
-                    HStack {
-                        URLImage(URL(string: friend.avatarUrl)!) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                        }.frame(width: AVATAR_HEIGHT, height: AVATAR_HEIGHT)
-                            .clipShape(Circle()).padding([.trailing], 20)
-                        
-                        Text(friend.name)
-                        
-                        
-                        Spacer()
-                        
-                        Circle().stroke(Color.gray, lineWidth: 2).frame(width: AVATAR_HEIGHT, height: AVATAR_HEIGHT).onTapGesture {
-                            print("CLICK 2")
-                        }
-                        
-                        Circle().stroke(Color.orange, lineWidth: 2).frame(width: AVATAR_HEIGHT, height: AVATAR_HEIGHT).onTapGesture {
-                            print("CLICK")
-                        }
-                    }
+                    friendItem(friend: friend,
+                               showButtons: true)
                 }
             }
             
             Section(header: Text("FRIENDS")) {
                 ForEach(viewModel.friends) { friend in
-                    HStack {
-                        URLImage(URL(string: friend.avatarUrl)!) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                        }.frame(width: AVATAR_HEIGHT, height: AVATAR_HEIGHT)
-                            .clipShape(Circle()).padding([.trailing], 20)
-                        
-                        Text(friend.name)
-                    }
+                    friendItem(friend: friend,
+                               showButtons: false)
                 }
             }.listStyle(GroupedListStyle())
         }
+    }
+}
+
+private struct friendItem: View {
+    let friend: Friend
+    let showButtons: Bool
+
+    var body: some View {
+        HStack {
+            URLImage(URL(string: friend.avatarUrl)!) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }.frame(width: AVATAR_HEIGHT, height: AVATAR_HEIGHT)
+                .clipShape(Circle()).padding([.trailing], 20)
+
+            Text(friend.name)
+
+            if showButtons {
+                Spacer()
+
+                CircleButton(color: Color.gray, icon: .cross)
+                CircleButton(color: Color.orange, icon: .check)
+            }
+        }
+    }
+}
+
+private enum CircleButtonType {
+    case check
+    case cross
+}
+
+private struct CircleButton: View {
+    let color: Color
+    let icon: CircleButtonType
+
+    var typeString: String {
+        switch icon {
+        case .check:
+            return "checkmark.circle"
+        case .cross:
+            return "x.circle"
+        }
+    }
+
+    var body: some View {
+        Button(action: {
+            print("CLICK \(self.typeString)")
+        }) {
+            Image(systemName: typeString)
+                .resizable()
+                .padding(5)
+                .frame(width: AVATAR_HEIGHT,
+                       height: AVATAR_HEIGHT)
+        }
+        .buttonStyle(PlainButtonStyle())
+        .foregroundColor(color)
     }
 }
 
