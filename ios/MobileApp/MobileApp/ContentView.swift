42f8f766b3c185e6de2616e61416ae731c65729c
Pontus Malm <malm@chalmers.it>
2021-11-28 17:29:16 +0100

    MapComponent

--- ios/MobileApp/MobileApp/ContentView.swift
+++ ios/MobileApp/MobileApp/ContentView.swift
@@ -1,3 +1,4 @@
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
@@ -10,7 +11,7 @@ struct ContentView: View {
                     Image(systemName: "cup.and.saucer.fill")
                     Text("Cheers")
                 }
-            Text("This is map!")
+            CheersMap()
                 .tabItem {
                     Image(systemName: "map.fill")
                     Text("Map")
@@ -31,6 +32,14 @@ struct ContentView: View {
                     Text(viewModel.username)
                 }
         }
+    }
+}
+
+private struct CheersMap: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 57.708870, longitude: 11.974560), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+
+    var body: some View {
+        Map(coordinateRegion: $region)
     }
 }
 
