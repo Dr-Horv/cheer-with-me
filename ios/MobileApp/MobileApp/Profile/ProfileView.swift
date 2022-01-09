2d1e86f35047661d680a159306712d47b143b659
Johan Lindskogen <johan.lindskogen@gmail.com>
2022-01-09 21:53:27 +0100

    format: reindent profile view

--- ios/MobileApp/MobileApp/Profile/ProfileView.swift
+++ ios/MobileApp/MobileApp/Profile/ProfileView.swift
@@ -6,23 +6,23 @@ import Alamofire
 struct ProfileView: View {
     @ObservedObject var viewModel: MainViewModel
     let friend = Friend(id: 2, name: "Ndushierino", avatarUrl: "https://randomuser.me/api/portraits/men/90.jpg")
-
+    
     var body: some View {
         VStack {
             List {
                 HStack {
                     if let avatarUrl = friend.avatarUrl {
-                         URLImage(URL(string: avatarUrl)!) { image in
-                             image
-                                 .resizable()
-                                 .aspectRatio(contentMode: .fit)
-                         }.frame(width: AVATAR_HEIGHT, height: AVATAR_HEIGHT)
-                             .clipShape(Circle()).padding([.trailing], 20)
-                     } else {
-                         Circle()
-                             .frame(width: AVATAR_HEIGHT, height: AVATAR_HEIGHT)
-                                 .clipShape(Circle()).padding([.trailing], 20)
-                     }
+                        URLImage(URL(string: avatarUrl)!) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }.frame(width: AVATAR_HEIGHT, height: AVATAR_HEIGHT)
+                            .clipShape(Circle()).padding([.trailing], 20)
+                    } else {
+                        Circle()
+                            .frame(width: AVATAR_HEIGHT, height: AVATAR_HEIGHT)
+                            .clipShape(Circle()).padding([.trailing], 20)
+                    }
                     Text(friend.name)
                     Spacer()
                 }
@@ -36,7 +36,7 @@ struct ProfileView: View {
             }
         }.onAppear {
             AF.request("\(BACKEND_URL)/user/me", headers: SingletonState.shared.authHeaders()).responseDecodable(of: User.self) { response in
-
+                
                 if let me = response.value {
                     debugPrint(me)
                     friend = me
