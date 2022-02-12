d5263fcf9372a932c5a410d2aada7f42b64d3cd3
Pontus Malm <malm@chalmers.it>
2022-02-12 16:07:08 +0100

    refactor: Events logic in view model

--- ios/MobileApp/MobileApp/Events/EventsView.swift
+++ ios/MobileApp/MobileApp/Events/EventsView.swift
@@ -2,19 +2,17 @@ import SwiftUI
 import Alamofire
 import MapKit
 
-
 struct SingleEventMapView: View {
     let coordinate: CLLocationCoordinate2D
 
-    
     func markers() -> [EventMarker] {
         return [
             EventMarker(coordinate: coordinate)
         ]
     }
-    
+
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
-    
+
     var body: some View {
         Map(coordinateRegion: $region, interactionModes: [], annotationItems: self.markers()) { item in
             MapMarker(coordinate: item.coordinate, tint: .blue)
@@ -30,17 +28,16 @@ struct EventMarker: Identifiable {
     let coordinate: CLLocationCoordinate2D
 }
 
-
 struct SingleEventView: View {
     let event: Happening
-    
+
     func coord() -> CLLocationCoordinate2D? {
         if let c = event.location?.coordinate {
             return CLLocationCoordinate2D(latitude: c.lat, longitude: c.lng)
         }
         return nil
     }
-    
+
     var body: some View {
         VStack {
             Text(event.description)
@@ -58,68 +55,39 @@ enum DateError: String, Error {
     case invalidDate
 }
 
-
 struct EventsView: View {
-    @State var isLoading = false
-    @State var happenings: [Happening] = []
-    
+    @ObservedObject var viewModel = EventsViewModel()
+
     var body: some View {
         NavigationView {
             VStack {
-                if isLoading {
+                if viewModel.isLoading {
                     ProgressView()
                 }
-                List(happenings) { h in
+                List(viewModel.happenings) { h in
                     NavigationLink(destination: SingleEventView(event: h)) {
                         Text(h.name)
                     }
                 }
-            }.navigationTitle("Events")
-                .onAppear {
-                    isLoading = true
-
-                    let formatter = DateFormatter()
-                    formatter.calendar = Calendar(identifier: .iso8601)
-                    formatter.locale = Locale(identifier: "en_US_POSIX")
-                    formatter.timeZone = TimeZone(secondsFromGMT: 0)
-                    
-                    let decoder = JSONDecoder()
-                    
-                    decoder.dateDecodingStrategy = .custom {
-                        (decoder) -> Date in
-                            let container = try decoder.singleValueContainer()
-                            let dateStr = try container.decode(String.self)
-
-                            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
-                            if let date = formatter.date(from: dateStr) {
-                                return date
-                            }
-                            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
-                            if let date = formatter.date(from: dateStr) {
-                                return date
-                            }
-                            throw DateError.invalidDate
-                    }
-                    
-                    AF.request("\(BACKEND_URL)/happenings", headers: SingletonState.shared.authHeaders()).responseDecodable(of: [Happening].self, decoder: decoder) {
-                        response in
-                        
-                        debugPrint(response)
-                        
-                        isLoading = false
-                        
-                        if let data = response.value {
-                            happenings = data
-                            debugPrint(9)
-                        }
-                    }
-                }
+            }
+            .navigationTitle("Events")
+            .onAppear {
+                viewModel.getEvents()
+            }
         }
     }
 }
 
 struct EventsView_Previews: PreviewProvider {
     static var previews: some View {
-        EventsView()
+        EventsView(viewModel: .example)
+    }
+}
+
+struct SingleEventView_Previews: PreviewProvider {
+    static var previews: some View {
+        SingleEventView(
+            event: EventsViewModel.example.happenings.last!
+        )
     }
 }
--- ios/MobileApp/MobileApp/Events/EventsViewModel.swift
+++ ios/MobileApp/MobileApp/Events/EventsViewModel.swift
@@ -1,6 +1,5 @@
+import Alamofire
 import Foundation
-
-
 
 struct Coordinate: Codable {
     let lat: Double
@@ -30,9 +29,92 @@ struct User: Identifiable, Codable {
     let avatarUrl: String?
 }
 
-
 struct FriendsResponse: Codable {
     let friends: [User]
     let incomingFriendRequests: [User]
     let outgoingFriendRequests: [User]
+}
+
+class EventsViewModel: ObservableObject {
+    @Published var isLoading = false
+    @Published var happenings: [Happening] = []
+    private var decoder = getDecoder()
+
+    func getEvents() {
+        isLoading = true
+
+        AF.request("\(BACKEND_URL)/happenings", headers: SingletonState.shared.authHeaders()).responseDecodable(of: [Happening].self, decoder: decoder) {
+            response in
+
+            debugPrint(response)
+
+            self.isLoading = false
+
+            if let data = response.value {
+                self.happenings = data
+                debugPrint(9)
+            }
+        }
+    }
+}
+
+private func getDecoder() -> JSONDecoder {
+    let formatter = DateFormatter()
+    formatter.calendar = Calendar(identifier: .iso8601)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+
+    let decoder = JSONDecoder()
+
+    decoder.dateDecodingStrategy = .custom {
+        (decoder) -> Date in
+            let container = try decoder.singleValueContainer()
+            let dateStr = try container.decode(String.self)
+
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+            if let date = formatter.date(from: dateStr) {
+                return date
+            }
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
+            if let date = formatter.date(from: dateStr) {
+                return date
+            }
+            throw DateError.invalidDate
+    }
+
+    return decoder
+}
+
+extension EventsViewModel {
+    static var example : EventsViewModel {
+        let viewModel = EventsViewModel()
+        viewModel.happenings = exampleHappenings()
+        return viewModel
+    }
+}
+
+private func exampleHappenings() -> [Happening] {
+    let malt = User(id: 5, nick: "Malt", avatarUrl: nil)
+    let horv = User(id: 6, nick: "Alkohorv", avatarUrl: nil)
+    let oneDay = TimeInterval(12*60*60)
+    let codeParty = Happening(happeningId: "theCoding",
+                              admin: malt,
+                              name: "Do the code",
+                              description: "Code code, drink beer",
+                              time: Date(timeIntervalSinceNow: TimeInterval(5*oneDay)),
+                              location: nil,
+                              attendees: [malt, horv],
+                              awaiting: [],
+                              cancelled: false)
+
+    let beerParty = Happening(happeningId: "beerOClock",
+                              admin: horv,
+                              name: "Drink the beer",
+                              description: "Bring your own beer. Drink beer",
+                              time: Date(timeIntervalSinceNow: TimeInterval(6*oneDay)),
+                              location: nil,
+                              attendees: [malt, horv],
+                              awaiting: [],
+                              cancelled: false)
+    return [codeParty, beerParty]
 }
