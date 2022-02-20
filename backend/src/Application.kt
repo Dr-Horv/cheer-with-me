aa5fe65c1909b829b62306145173fc67db6539fc
Johan Horvius <drhorv@gmail.com>
2022-02-20 21:24:33 +0100

    feat(backend/timestamps): Make timestamp always be millisecond resolution
    Refs: #49

--- backend/src/Application.kt
+++ backend/src/Application.kt
@@ -3,7 +3,9 @@ package dev.fredag.cheerwithme
 import com.auth0.jwk.JwkProviderBuilder
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.util.StdDateFormat
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import dev.fredag.cheerwithme.model.AppleOauthResponse
@@ -31,6 +33,8 @@ import org.slf4j.event.Level
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.sns.SnsClient
 import java.net.URL
+import java.time.Instant
+import java.time.format.DateTimeFormatterBuilder
 import java.util.concurrent.TimeUnit
 
 
@@ -54,6 +58,9 @@ private val searchService: SearchService = SearchService(userService, friendsSer
 
 private val logger = LoggerFactory.getLogger("Application.kt")
 
+class InstantSerializerWithMilliSecondPrecision :
+    InstantSerializer(INSTANCE, false, DateTimeFormatterBuilder().appendInstant(3).toFormatter())
+
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
@@ -61,7 +68,9 @@ fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT)
-            registerModule(JavaTimeModule())
+            val timeModule = JavaTimeModule()
+            timeModule.addSerializer(Instant::class.java, InstantSerializerWithMilliSecondPrecision())
+            registerModule(timeModule)
             disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         }
     }
